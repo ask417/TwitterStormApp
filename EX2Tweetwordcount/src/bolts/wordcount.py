9a2492c891a23a555b842643b5abaf64e4ceead8
root <root@ip-172-31-58-85.ec2.internal>
2016-03-28 00:35:29 +0000

    Postgres queries in wordcount bolt

--- EX2Tweetwordcount/src/bolts/wordcount.py
+++ EX2Tweetwordcount/src/bolts/wordcount.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import Counter
 from streamparse.bolt import Bolt
-
+#import psycopg2
 
 
 class WordCounter(Bolt):
@@ -10,20 +10,24 @@ class WordCounter(Bolt):
     def initialize(self, conf, ctx):
         self.counts = Counter()
         self.redis = StrictRedis()
+	#self.conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
 
     def process(self, tup):
         word = tup.values[0]
 
-        # Write codes to increment the word count in Postgres
-        # Use psycopg to interact with Postgres
-        # Database name: Tcount 
-        # Table name: Tweetwordcount 
-        # you need to create both the database and the table in advance.
-        
-
-        # Increment the local count
+	# Increment the local count
         self.counts[word] += 1
         self.emit([word, self.counts[word]])
+	#cur = self.conn.cursor()
+	#cur.execute("select * from tweetwordcount where word=%s", (word))
+	#record = cur.fetchall()
 
+	#if len(record)==0:
+	#	cur.execute("insert into tweetwordcount (word,count) values (%s, 1)",(word))
+	#else:
+	#	cur.execute("update tweetwordcount set count=%s where word=%s",(count,word))
+
+	#self.conn.commit()
+	#self.conn.close()
         # Log the count - just to see the topology running
         self.log('%s: %d' % (word, self.counts[word]))
